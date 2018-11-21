f6dace9f8a5a402314e66573f9cc660333707052
mapache.guru.com <01joilefille@gmail.com>
2018-11-21 15:31:40 +0100

    fin seccion 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+/*como funcionan las rutas:*/
+const app_routes: Routes = [
+  /*para el index:*/
+  {path: 'home', component: PortafolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'item', component: ItemComponent},
+  /*cualquier otra ruta que no sea las anteriores redireccionara a portafolio component*/
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(app_routes, {useHash: true})],
+  /*cuando no puedes modificarel htacces agregar , {useHash: true}) */
+  /*EL FORcHILD ES PARA RUTAS HIJAS */
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
